e418d7b047169defd8e4d679d98044e78ca64c02
Tanisall <tanisalhariroh96821@gmail.com>
2024-05-13 19:55:42 +0700

    Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUser } from "./redux/slices/userSlice";
+import { increment, decrement, incrementByAmount } from './redux/slices/counterSlice';
 
 function UserComponent() {
   const userData = useSelector((state) => state.user.data);
   const isLoading = useSelector((state) => state.user.loading);
+  const count = useSelector(state => state.counter.value);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,17 +20,48 @@ function UserComponent() {
   console.log(userData);
 
   return (
-    <div>
-      <h2>User Details</h2>
-      {userData?.map((item) => (
-        <div key={item.id}>
-          <span>Name: {item.name}</span>
-          <span> - </span>
-          <span>Email: {item.email}</span>
+    <div style={paddingContainer}>
+      <div style={columnContainer}>
+        <div>
+          <h2>User Details</h2>
+          {userData?.map((item) => (
+            <div key={item.id}>
+              <span>Name: {item.name}</span>
+              <span> - </span>
+              <span>Email: {item.email}</span>
+            </div>
+          ))}
         </div>
-      ))}
+        <div>
+          <h2>Counter</h2>
+          <div>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <span>{count}</span>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+          </div>
+          <div>
+            <input
+              type="number"
+              onChange={(e) => dispatch(incrementByAmount(parseInt(e.target.value) || 0))}
+              placeholder="Enter value"
+            />
+            <button onClick={() => dispatch(incrementByAmount(5))}>Add 5</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
+}
+
+const paddingContainer = {
+  paddingLeft: '20%',
+  paddingRight: '20%',
+  paddingTop: '5%'
+}
+
+const columnContainer = {
+  display: 'flex',
+  justifyContent: 'space-between',
 }
 
 export default UserComponent;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,3 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import counterReducer from './slices/counterSlice';
-// const store = configureStore({
-// reducer: {
-// counter: counterReducer
-// }
-// });
-// export default store;
-
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './slices/counterSlice';
 import userReducer from './slices/userSlice';
