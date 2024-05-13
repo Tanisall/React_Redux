d28f4d0762e65b988a51b467c6adbd5d11244a66
Tanisall <tanisalhariroh96821@gmail.com>
2024-05-13 16:40:10 +0700

    Redux React

---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import counterReducer from './slices/counterSlice';
+// const store = configureStore({
+// reducer: {
+// counter: counterReducer
+// }
+// });
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './slices/counterSlice';
+import userReducer from './slices/userSlice';
+const store = configureStore({
+reducer: {
+counter: counterReducer,
+user: userReducer,
+}
+});
+export default store;
